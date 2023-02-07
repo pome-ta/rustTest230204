83ea415f3cced081d0eab44396469e877f6663bf
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-08 01:13:40 +0900

    fix

--- rustSandbox/main.js
+++ rustSandbox/main.js
@@ -1,6 +1,8 @@
 fn main() {
-
-    let c:u32 = 32; // 32ビット符号なし整数型
-    let buf:u32 = 0xbu32;
+    let c: u32 = 32; // 32ビット符号なし整数型
+    let buf: u32 = 0xbu32;
     println!("😇 Hello CodeSandbox!");
+    println!("{}", c);
+    println!("{}", buf);
 }
+
