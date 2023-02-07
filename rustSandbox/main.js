4661286d2ea240589a19c26498d0fe49573ea841
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-08 01:12:33 +0900

    0xbu32

---
+++ rustSandbox/main.js
@@ -0,0 +1,6 @@
+fn main() {
+
+    let c:u32 = 32; // 32ビット符号なし整数型
+    let buf:u32 = 0xbu32;
+    println!("😇 Hello CodeSandbox!");
+}
---
+++ rustSandbox/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("😇 Hello CodeSandbox!");
+}
