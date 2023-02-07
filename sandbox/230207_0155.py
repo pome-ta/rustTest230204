849dfa4e690b287e8635b62e0061cfed3207f8cc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-07 14:05:59 +0900

    decimal

--- sandbox/230207_0155.py
+++ sandbox/230207_0155.py
@@ -2,6 +2,12 @@ from functools import reduce
 
 binarius = 10111001
 #bin_array = reversed([int(b) for b in str(binarius)])
-bin_array = [[n,int(b)] for n, b in enumerate(str(binarius)[::-1])]
+#bin_array = [[n,int(b)] for n, b in enumerate(str(binarius)[::-1])]
 
-#to_decimal = 
+to_decimal = 0
+
+for n, b in enumerate(str(binarius)[::-1]):
+  to_decimal += int(b) * pow(2, n)
+
+print(to_decimal)
+print(0b10111001)
