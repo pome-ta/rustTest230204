15f2d395864315bdb0d5942d16dc96d37ef5349c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-10 01:37:32 +0900

    up

---
+++ pySandbox/230210_0136.py
@@ -0,0 +1,30 @@
+# todo: 浮.動小数点数処理する
+
+
+def print_result(index, num):
+  u32 = f'{num:032b}' [-32:]  # オーバーフローとして
+  result = f'[{index}]: {u32}\t{num}'
+  print(result)
+
+
+def binary_output(num_list):
+  list_index = reversed(range(len(num_list)))
+  for i, n in zip(list_index, num_list[::-1]):
+    print_result(i, n)
+
+
+if __name__ == '__main__':
+  u_time = 110
+
+  value_list = [
+    u_time,
+    0xb,
+    9,
+    0xb ^ 9,
+    0xffffffff,
+    0xffffffff + u_time,
+    11.5625,
+  ]
+
+  binary_output(value_list)
+
