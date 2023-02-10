76ad1f227fac53932cd3062316e67e118d26e1ce
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-10 17:50:48 +0900

    float bin

--- pySandbox/230210_0136.py
+++ pySandbox/230210_0136.py
@@ -1,4 +1,5 @@
 import struct
+
 # [Pythonで浮動小数点数floatと16進数表現の文字列を相互に変換 | note.nkmk.me](https://note.nkmk.me/python-float-hex/)
 
 
@@ -8,11 +9,17 @@ def float_to_hex(f):
 
 # todo: 浮動小数点数処理する
 def floatBitsToUint(num: float) -> str:
-  pass
+  sign_is = 1 if num < 0.0 else 0
+  hex_str = float_to_hex(num)
+  bin_str = bin(int(hex_str, 16))[2:]
+  return str(sign_is) + bin_str
 
 
 def print_result(index, num):
-  u32 = f'{num:032b}' [-32:]  # オーバーフローとして
+  if isinstance(num, int):
+    u32 = f'{num:032b}' [-32:]  # オーバーフローとして
+  elif isinstance(num, float):
+    u32 = floatBitsToUint(num)
   result = f'[{index}]: {u32}\t{num}'
   print(result)
 
@@ -21,6 +28,10 @@ def binary_output(num_list):
   list_index = reversed(range(len(num_list)))
   for i, n in zip(list_index, num_list[::-1]):
     print_result(i, n)
+
+
+def header_footer():
+  pass
 
 
 if __name__ == '__main__':
@@ -33,13 +44,20 @@ if __name__ == '__main__':
     0xb ^ 9,
     0xffffffff,
     0xffffffff + u_time,
-    130,
+    11.5625,
   ]
 
-  #binary_output(value_list)
-  h = float_to_hex(11.5625)
-  i = int(h, 16)
-  b = bin(int(h, 16))
-  
+  binary_output(value_list)
+  #h = float_to_hex(11.5625)
+  #i = int(h, 16)
+  #b = bin(int(h, 16))
+  #bb = b[2:]
+  #bbb = '0' + bb
+  f2u = floatBitsToUint(11.5625)
+  b = '0b' + f2u
+  q = int(b, 0)
+  b8 = struct.pack("I", q)
+  #print(struct.unpack("f", b8)[0])
+
   #print(bin(h))
 
