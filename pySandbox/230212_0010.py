fb15bb8268544f75dd700fadbf5b7b864c49e80a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-12 11:17:29 +0900

    up

--- pySandbox/230212_0010.py
+++ pySandbox/230212_0010.py
@@ -4,6 +4,33 @@ k = 0x456789ab
 UINT_MAX = 0xffffffff
 
 
+
+def set_index(func):
+  # xxx: 色々と酷いデコレータ
+  def _index(_i, _n):
+    _b = int(_i + _n).to_bytes(1, byteorder='big')
+    return str(_b)[-2:-1]
+
+  b_list = [_index(i, 48) if i < 10 else _index(i, 55) for i in range(32)]
+  out = 'idx: '
+  for n, b in enumerate(b_list):
+    out += '|' + b if n == 1 or n == 9 else b
+
+  lngth = 42
+  separate = ''.join(['=' for _ in range(lngth)])
+  bar = ''.join(['-' for _ in range(lngth)])
+
+  def wrapper(*args, **kwargs):
+    print(separate)
+    print(out)
+    print(bar)
+    func(*args, **kwargs)
+    print(bar)
+    #print(out)
+
+  return wrapper
+
+
 def float_to_hex(f: float) -> str:
   """
   [Pythonで浮動小数点数floatと16進数表現の文字列を相互に変換 | note.nkmk.me](https://note.nkmk.me/python-float-hex/)
@@ -39,6 +66,15 @@ def uhash11(n):
   nk = n * k
   return nk
 
+def uuu(n):
+  n ^= (n << 1)
+  n ^= (n >> 1)
+  n *= k
+  n ^= (n << 1)
+  print(n)
+  
+  return n
+
 
 def uint_to_float(u_num: int) -> float:
   b8 = struct.pack('f', u_num)
@@ -51,7 +87,36 @@ def hash11(p: float):
   return uhash11(n) / UINT_MAX
 
 
-h = hash11(1.0)
+def number_to_binary(num) -> str:
+  if isinstance(num, int):
+    u32 = f'{num:032b}' [-32:]  # オーバーフローとして
+  elif isinstance(num, float):
+    u32 = _floatBitsToUint(num)
+  return u32
+
+
+def print_result(index: int, binary: str, num=''):
+  row = ''
+  for n, i in enumerate(binary):
+    row += '|' + i if n == 1 or n == 9 else i
+  result = f'[{index}]: {row}\t{num}'
+  print(result)
+
+
+@set_index
+def binary_output(num_list):
+  list_index = reversed(range(len(num_list)))
+  for i, n in zip(list_index, num_list[::-1]):
+    b = number_to_binary(n)
+    #print_result(i, b, n)
+    print_result(i, b)
+
+h = uuu(floatBitsToUint(1.0))
+
+binary_output([h])
+
+# 2回目 `n ^= (n << 1)`
+#1|01000001|10000000000000000000000
 
 
 x = 1
