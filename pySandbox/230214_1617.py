411e31074927b10ca9b90346e063549d7265f7a4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-14 20:35:03 +0900

    up

--- pySandbox/230214_1617.py
+++ pySandbox/230214_1617.py
@@ -1,3 +1,19 @@
-from ctypes import c_uint32
+import ctypes
+import struct
+import array
 
-f2u = c_uint32(0xffff_ffff + 1)
+
+def uint32(num: int) -> int:
+  c_uint32 = ctypes.c_uint32(abs(num))
+  return c_uint32.value
+
+
+num_max = 0xffff_ffff
+
+f2u = ctypes.c_uint32(num_max)
+
+#sp = struct.pack('I', num_max)
+sp = struct.pack('I', f2u.value)
+
+record_size = struct.calcsize('I')
+
