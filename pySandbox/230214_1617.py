fe414f68c69b12d7f788958b82982b182eb3409a
pome-ta <pometarou93@gmail.com>
2023-02-14 22:55:41 +0900

    up

--- pySandbox/230214_1617.py
+++ pySandbox/230214_1617.py
@@ -3,7 +3,6 @@ import struct
 import array
 
 
-
 def uint32(num: int) -> int:
   c_uint32 = ctypes.c_uint32(abs(num))
   return c_uint32.value
@@ -14,9 +13,8 @@ num_max = 0xffff_ffff
 f2u = ctypes.c_uint32(num_max)
 uu = uint32(110)
 
-#sp = struct.pack('I', num_max)
+# sp = struct.pack('I', num_max)
 format = 'B' * 32
 sp = struct.pack('>I', uu)
 
 record_size = struct.calcsize('I')
-
