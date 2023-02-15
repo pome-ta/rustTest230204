88704a6d433b7c0caee6447cd52cf61868061176
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-15 21:11:52 +0900

    up

--- pySandbox/230215_1701.py
+++ pySandbox/230215_1701.py
@@ -4,14 +4,63 @@ import struct
 import math
 import array
 
+k = 0x456789ab
+UINT_MAX = 0xffffffff
+
+
+def set_index(func):
+  # xxx: 色々と酷いデコレータ
+  def _index(_i, _n):
+    _b = int(_i + _n).to_bytes(1, byteorder='big')
+    return str(_b)[-2:-1]
+
+  b_list = [_index(i, 48) if i < 10 else _index(i, 55) for i in range(32)]
+  out = 'idx:'
+  for n, b in enumerate(b_list):
+    out += '|' + b if n == 1 or n == 9 else b
+    out += '_' if (n + 1) % 4 == 0 else ''
+
+  lngth = 42
+  separate = ''.join(['=' for _ in range(lngth)])
+  bar = ''.join(['-' for _ in range(lngth)])
+
+  def wrapper(*args, **kwargs):
+    print(separate)
+    print(out)
+    print(bar)
+    func(*args, **kwargs)
+    print(bar)
+    # print(out)
+
+  return wrapper
+
+
+def print_result(index: int, binary, num=''):
+  row = ''
+  binary_str = f'{binary:032b}'
+  for n, i in enumerate(binary_str):
+    row += '|' + i if n == 1 or n == 9 else i
+    row += '' if not(n) or n % 4 != 0 else '_'
+  result = f'[{index}]:{row}\t{num}'
+  print(result)
+
+
+@set_index
+def binary_output(num_list):
+  list_index = reversed(range(len(num_list)))
+  for i, n in zip(list_index, num_list[::-1]):
+    print_result(i, n)
+
 
 def uint32(s) -> int:
   _c_uint32 = ctypes.c_uint32(int(s))
   return _c_uint32.value
 
-def float32(f)->float:
+
+def float32(f) -> float:
   _c_float = ctypes.c_float(f)
   return _c_float.value
+
 
 def floatBitsToUint(f: float) -> int:
   fp = struct.pack('>f', f)
@@ -37,16 +86,9 @@ num_list = [
   floatBitsToUint(11.5625),
 ]
 
-
 uint_list = [uint_set(i) for i in num_list]
 
-
-for n, u in enumerate(uint_list[::-1]):
-  sf32 = f'{u:032b}'
-  print(len(uint_list) - n - 1, sf32)
-
-
-
+binary_output(uint_list)
 
 x = 1
 
