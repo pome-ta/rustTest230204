24160a1327b20f6b79b0012581b4e6d525d831dc
pome-ta <pometarou93@gmail.com>
2023-02-16 00:26:07 +0900

    refacter

--- pySandbox/230215_1701.py
+++ pySandbox/230215_1701.py
@@ -4,52 +4,56 @@ import struct
 import math
 import array
 
+
 k = 0x456789ab
 UINT_MAX = 0xffffffff
 
 
+def reference_grid(div_chrs: str,  head_str: str = '') -> str:
+  row = head_str
+  for n, c in enumerate(div_chrs):
+    row += '|' + c if n == 1 or n == 9 else c
+    row += '_' if n and (n + 1) % 4 == 0 else ''
+  return row[:-1]  # xxx: お尻`_` 切り捨て
+
+
 def set_index(func):
-  # xxx: 色々と酷いデコレータ
-  def _index(_i, _n):
-    _b = int(_i + _n).to_bytes(1, byteorder='big')
-    return str(_b)[-2:-1]
+  # xxx: 色々と酷いデコレータ、そしてlambda
+  to_0v = (lambda n, j: int(n + j).to_bytes(1, byteorder='big'))
+  repeat_chrs = (lambda chr, l: ''.join(chr for _ in range(l)))
 
-  b_list = [_index(i, 48) if i < 10 else _index(i, 55) for i in range(32)]
-  out = 'idx:'
-  for n, b in enumerate(b_list):
-    out += '|' + b if n == 1 or n == 9 else b
-    out += '_' if (n + 1) % 4 == 0 else ''
-
-  lngth = 42
-  separate = ''.join(['=' for _ in range(lngth)])
-  bar = ''.join(['-' for _ in range(lngth)])
+  zoro2v = [to_0v(i, 48).decode('utf8') if i <
+            10 else to_0v(i, 55).decode('utf8') for i in range(32)]
+  hd = 'inx :'
+  lngth = 48
 
   def wrapper(*args, **kwargs):
-    print(separate)
-    print(out)
-    print(bar)
-    func(*args, **kwargs)
-    print(bar)
-    # print(out)
-
+    output_list = [
+        repeat_chrs('=', lngth),
+        reference_grid(zoro2v, hd),
+        repeat_chrs('-', lngth),
+        func(*args, **kwargs),
+        repeat_chrs('-', lngth),
+    ]
+    return '\n'.join(output_list)
   return wrapper
 
 
 def print_result(index: int, binary, num=''):
-  row = ''
+  hd = f'[{index}] :'
   binary_str = f'{binary:032b}'
-  for n, i in enumerate(binary_str):
-    row += '|' + i if n == 1 or n == 9 else i
-    row += '' if not(n) or n % 4 != 0 else '_'
-  result = f'[{index}]:{row}\t{num}'
-  print(result)
+  row = reference_grid(binary_str)
+  result = f'{hd}{row}\t{num}'
+  return result
 
 
 @set_index
 def binary_output(num_list):
+  output_list: list = []
   list_index = reversed(range(len(num_list)))
   for i, n in zip(list_index, num_list[::-1]):
-    print_result(i, n)
+    output_list.append(print_result(i, n))
+  return '\n'.join(output_list)
 
 
 def uint32(s) -> int:
@@ -75,20 +79,20 @@ def uint_set(num) -> int:
 t = 110.9
 
 num_list = [
-  uint32(t),
-  uint32(-t),
-  floatBitsToUint(t),
-  0xb ^ 9,
-  0xffffffff,
-  0xffffffff + uint32(t),
-  floatBitsToUint(math.floor(t)),
-  floatBitsToUint(math.floor(-t)),
-  floatBitsToUint(11.5625),
+    uint32(t),
+    uint32(-t),
+    floatBitsToUint(t),
+    0xb ^ 9,
+    0xffffffff,
+    0xffffffff + uint32(t),
+    floatBitsToUint(math.floor(t)),
+    floatBitsToUint(math.floor(-t)),
+    floatBitsToUint(11.5625),
 ]
 
 uint_list = [uint_set(i) for i in num_list]
 
-binary_output(uint_list)
+hoge = binary_output(uint_list)
 
+print(hoge)
 x = 1
-
