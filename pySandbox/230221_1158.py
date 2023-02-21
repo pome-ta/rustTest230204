47b46a5515cbea7619f3c51de757c8da5658defb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-21 14:58:54 +0900

    up

--- pySandbox/230221_1158.py
+++ pySandbox/230221_1158.py
@@ -1,5 +1,20 @@
 import numpy as np
+from PIL import Image as ImageP
 
 UINT_MAX = 0xffffffff
 
-u = np.array([UINT_MAX+1], dtype=np.uint32)
+u = np.array([UINT_MAX + 1], dtype=np.uint32)
+
+sq_size = 8
+np_canvas = np.zeros((sq_size, sq_size, 3), dtype=np.float32)
+
+p_canvas = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+
+add_np = np.
+
+p_canvas += 1
+p_canvas **= 2
+
+#init_img = ImageP.new('RGB', (sq_size, sq_size))
+#base_array = np.asarray(init_img)
+imgp = ImageP.fromarray(p_canvas)
