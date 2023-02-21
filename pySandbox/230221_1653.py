4ed329ddcec95817a0754b00647638b2ec5cf1aa
pome-ta <pometarou93@gmail.com>
2023-02-21 22:47:59 +0900

    up

--- pySandbox/230221_1653.py
+++ pySandbox/230221_1653.py
@@ -1,8 +1,14 @@
 import numpy as np
+from PIL import Image as ImageP
+
+
+UINT_MAX = 0xffffffff
 
 sq_size = 8
 RGB_SIZE = 256
 
-xy = np.mgrid[0:sq_size:RGB_SIZE/sq_size, 0:sq_size:RGB_SIZE/sq_size]
+# xyz = np.arange(0, RGB_SIZE, RGB_SIZE/sq_size).reshape(sq_size, sq_size, 3)
+xyz = np.arange(0, 10)
+zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
 
-#xy = np.arange()
+_ = 1
