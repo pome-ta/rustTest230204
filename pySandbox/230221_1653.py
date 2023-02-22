d5a8384781060bde6141f51e7804ca4dd608bd72
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-22 10:40:08 +0900

    up

--- pySandbox/230221_1653.py
+++ pySandbox/230221_1653.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image as ImageP
 
-
 UINT_MAX = 0xffffffff
 
 sq_size = 64
@@ -10,10 +9,19 @@ color_ch = 3
 
 length_size = sq_size * sq_size * color_ch
 
-xyz = np.arange(0, length_size, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
+#xyz = np.arange(0, length_size, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
 #xyz = np.arange(0, 10)
-zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+#zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
 
+#xyz = np.arange(0, length_size,RGB_SIZE)  #.reshape(sq_size, sq_size, color_ch)
 
-imgp = ImageP.fromarray(xyz)
+#imgp = ImageP.fromarray(xyz)
+
+#print(sq_size/RGB_SIZE * length_size)
+
+ll = np.linspace(0, length_size)
+
+ll_size = ll.size
+
 _ = 1
+
